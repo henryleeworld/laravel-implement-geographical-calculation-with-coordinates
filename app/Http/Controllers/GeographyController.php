5c286f4b6d6454d7f410f1544a1264d47e2feef5
Henry Lee <henryleeworld@gmail.com>
2024-09-10 00:10:22 +0800

    feat:升級至 laravel 11

--- app/Http/Controllers/GeographyController.php
+++ app/Http/Controllers/GeographyController.php
@@ -3,19 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Arr;
-use KMLaravel\GeographicalCalculator\Facade\GeoFacade;
+use Location\Coordinate;
+use Location\Distance\Vincenty;
 
 class GeographyController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct(
+        protected Vincenty $vincenty
+    ) {}
+
     public function calculate() 
     {
-        $distance = GeoFacade::setPoint([25.047836840458658, 121.5174705708949])
-                    ->setPoint([22.688007863237587, 120.309479149447])
-                    ->setOptions(['units' => [$unit = 'km']])
-                    // you can set unlimited lat/long points.
-                    // get the calculated distance between each point
-                    ->getDistance();
-        // dd($distance);
-        echo '從台北車站到高鐵左營站距離 ' . Arr::get($distance, '1-2.' . $unit) . ' 公里' . PHP_EOL;
+        $coordinate1 = new Coordinate(25.047836840458658, 121.5174705708949);
+        $coordinate2 = new Coordinate(22.688007863237587, 120.309479149447);
+        echo __('Distance from Taipei Main Station to Taiwan High-Speed Rail Zuoying Station is :kilometers kilometers.', ['kilometers' => ($coordinate1->getDistance($coordinate2, $this->vincenty)) / 1000]) . PHP_EOL;
     }
 }
